c25300c6582a3c439e8ef57a1e52ab0d4446a2ef
Steveplays28 <djspaargaren@outlook.com>
2024-04-21 16:22:10 +0200

    feat: Implement entity unloading
    Death animations and item pickups work now.

---
+++ common/src/main/java/io/github/steveplays28/noisium/mixin/server/world/ServerEntityManagerMixin.java
@@ -0,0 +1,23 @@
+package io.github.steveplays28.noisium.mixin.server.world;
+
+import io.github.steveplays28.noisium.server.world.entity.event.NoisiumServerEntityEvent;
+import net.minecraft.entity.Entity;
+import net.minecraft.server.world.ServerEntityManager;
+import net.minecraft.world.entity.EntityLike;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(value = ServerEntityManager.class, priority = 500)
+public class ServerEntityManagerMixin<T extends EntityLike> {
+	@Inject(method = "stopTracking", at = @At(value = "HEAD"), cancellable = true)
+	private void noisium$entityRemoveEventImplementation(T entity, CallbackInfo ci) {
+		if (entity instanceof Entity entityInWorld) {
+			if (NoisiumServerEntityEvent.REMOVE.invoker().onServerEntityRemove(
+					entityInWorld, entityInWorld.getWorld()).interruptsFurtherEvaluation()) {
+				ci.cancel();
+			}
+		}
+	}
+}
--- common/src/main/java/io/github/steveplays28/noisium/server/world/entity/NoisiumServerWorldEntityTracker.java
+++ common/src/main/java/io/github/steveplays28/noisium/server/world/entity/NoisiumServerWorldEntityTracker.java
@@ -2,6 +2,7 @@ package io.github.steveplays28.noisium.server.world.entity;
 
 import dev.architectury.event.EventResult;
 import dev.architectury.event.events.common.EntityEvent;
+import io.github.steveplays28.noisium.server.world.entity.event.NoisiumServerEntityEvent;
 import io.github.steveplays28.noisium.server.world.event.NoisiumServerTickEvent;
 import net.minecraft.entity.Entity;
 import net.minecraft.network.packet.Packet;
@@ -9,6 +10,8 @@ import net.minecraft.server.network.EntityTrackerEntry;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -16,14 +19,17 @@ import java.util.function.Consumer;
 
 // TODO: Reimplement the rest of ServerEntityManager and replace it
 public class NoisiumServerWorldEntityTracker {
-	private final Map<Integer, EntityTrackerEntry> entityTrackerEntries;
-	private final Consumer<Packet<?>> packetSendConsumer;
+	private static final @NotNull Logger LOGGER = LoggerFactory.getLogger("Noisium Server World Entity Tracker");
 
-	public NoisiumServerWorldEntityTracker(Consumer<Packet<?>> packetSendConsumer) {
+	private final @NotNull Map<Integer, EntityTrackerEntry> entityTrackerEntries;
+	private final @NotNull Consumer<Packet<?>> packetSendConsumer;
+
+	public NoisiumServerWorldEntityTracker(@NotNull Consumer<Packet<?>> packetSendConsumer) {
 		this.entityTrackerEntries = new HashMap<>();
 		this.packetSendConsumer = packetSendConsumer;
 
 		EntityEvent.ADD.register(this::onEntityAdded);
+		NoisiumServerEntityEvent.REMOVE.register(this::onEntityRemoved);
 		NoisiumServerTickEvent.SERVER_ENTITY_MOVEMENT_TICK.register(this::onTick);
 	}
 
@@ -34,30 +40,34 @@ public class NoisiumServerWorldEntityTracker {
 		}
 
 		var entityType = entity.getType();
-		entityTrackerEntries.put(
-				entity.getId(),
-				new EntityTrackerEntry(
-						(ServerWorld) world, entity, entityType.getTrackTickInterval(), entityType.alwaysUpdateVelocity(),
-						packetSendConsumer
-				)
+		var entityTrackerEntry = new EntityTrackerEntry(
+				(ServerWorld) world, entity, entityType.getTrackTickInterval(), entityType.alwaysUpdateVelocity(),
+				packetSendConsumer
 		);
+		entityTrackerEntries.put(entity.getId(), entityTrackerEntry);
 
 		var players = ((ServerWorld) world).getPlayers();
 		for (int i = 0; i < players.size(); i++) {
-			entityTrackerEntries.get(entity.getId()).startTracking(players.get(i));
+			entityTrackerEntry.startTracking(players.get(i));
 		}
-
 		return EventResult.interruptTrue();
 	}
 
 	@SuppressWarnings("ForLoopReplaceableByForEach")
 	private EventResult onEntityRemoved(@NotNull Entity entity, @NotNull World world) {
+		var entityId = entity.getId();
+		var entityTrackerEntry = entityTrackerEntries.get(entityId);
+		if (entityTrackerEntry == null) {
+			LOGGER.error("Tried removing an entity's entity tracker, but it was null. Entity: {}", entity);
+			return EventResult.interruptDefault();
+		}
+
 		var players = ((ServerWorld) world).getPlayers();
 		for (int i = 0; i < players.size(); i++) {
-			entityTrackerEntries.get(entity.getId()).stopTracking(players.get(i));
+			entityTrackerEntry.stopTracking(players.get(i));
 		}
-		entityTrackerEntries.remove(entity.getId());
-		return EventResult.pass();
+		entityTrackerEntries.remove(entityId);
+		return EventResult.interruptDefault();
 	}
 
 	private void onTick() {
