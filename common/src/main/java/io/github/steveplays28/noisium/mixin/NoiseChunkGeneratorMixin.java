962cf88e16c8d370cefa735d7d72b0d0186cacdc
Steveplays28 <djspaargaren@outlook.com>
2024-07-04 14:56:23 +0200

    refactor: Move `NoiseChunkGenerator#method_38332` lambda into an `@Overwrite`
    This allows other mods, such as ishland's mods, to change the `Executor` of `NoiseChunkGenerator#populateNoise`'s `CompletableFuture`.

--- common/src/main/java/io/github/steveplays28/noisium/mixin/NoiseChunkGeneratorMixin.java
+++ common/src/main/java/io/github/steveplays28/noisium/mixin/NoiseChunkGeneratorMixin.java
@@ -1,21 +1,17 @@
 package io.github.steveplays28.noisium.mixin;
 
-import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.block.BlockState;
-import net.minecraft.util.Util;
 import net.minecraft.world.biome.source.BiomeSource;
 import net.minecraft.world.chunk.Chunk;
 import net.minecraft.world.chunk.ChunkSection;
 import net.minecraft.world.gen.StructureAccessor;
 import net.minecraft.world.gen.chunk.*;
 import net.minecraft.world.gen.noise.NoiseConfig;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-
-import java.util.concurrent.CompletableFuture;
 
 @Mixin(NoiseChunkGenerator.class)
 public abstract class NoiseChunkGeneratorMixin extends ChunkGenerator {
@@ -27,7 +23,7 @@ public abstract class NoiseChunkGeneratorMixin extends ChunkGenerator {
 	}
 
 	@Redirect(method = "populateNoise(Lnet/minecraft/world/gen/chunk/Blender;Lnet/minecraft/world/gen/StructureAccessor;Lnet/minecraft/world/gen/noise/NoiseConfig;Lnet/minecraft/world/chunk/Chunk;II)Lnet/minecraft/world/chunk/Chunk;", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/chunk/ChunkSection;setBlockState(IIILnet/minecraft/block/BlockState;Z)Lnet/minecraft/block/BlockState;"))
-	private BlockState noisium$populateNoiseWrapSetBlockStateOperation(ChunkSection chunkSection, int chunkSectionBlockPosX, int chunkSectionBlockPosY, int chunkSectionBlockPosZ, BlockState blockState, boolean lock) {
+	private BlockState noisium$populateNoiseWrapSetBlockStateOperation(@NotNull ChunkSection chunkSection, int chunkSectionBlockPosX, int chunkSectionBlockPosY, int chunkSectionBlockPosZ, @NotNull BlockState blockState, boolean lock) {
 		// Update the non empty block count to avoid issues with MC's lighting engine and other systems not recognising the direct palette storage set
 		// See ChunkSection#setBlockState
 		chunkSection.nonEmptyBlockCount += 1;
@@ -50,33 +46,33 @@ public abstract class NoiseChunkGeneratorMixin extends ChunkGenerator {
 		return blockState;
 	}
 
-	@Inject(method = "populateNoise(Lnet/minecraft/world/gen/chunk/Blender;Lnet/minecraft/world/gen/noise/NoiseConfig;Lnet/minecraft/world/gen/StructureAccessor;Lnet/minecraft/world/chunk/Chunk;)Ljava/util/concurrent/CompletableFuture;", at = @At(value = "RETURN", shift = At.Shift.BEFORE), cancellable = true)
-	private void noisium$populateNoiseInject(Blender blender, NoiseConfig noiseConfig, StructureAccessor structureAccessor, Chunk chunk, CallbackInfoReturnable<CompletableFuture<Chunk>> cir, @Local GenerationShapeConfig generationShapeConfig, @Local(ordinal = 0) int minimumY, @Local(ordinal = 1) int minimumYFloorDiv, @Local(ordinal = 2) int generationShapeHeightFloorDiv) {
-		if (generationShapeHeightFloorDiv <= 0) {
-			cir.setReturnValue(CompletableFuture.completedFuture(chunk));
-			return;
+	/**
+	 * @author Steveplays28
+	 * @reason Improved chunk locking and unlocking speed by getting the chunk section array directly from the chunk
+	 * and by replacing {@code foreach} with {@code fori}.
+	 */
+	@Overwrite
+	public @Nullable Chunk method_38332(@NotNull Chunk chunk, int generationShapeHeightFloorDiv, @NotNull GenerationShapeConfig generationShapeConfig, int minimumY, @NotNull Blender blender, @NotNull StructureAccessor structureAccessor, @NotNull NoiseConfig noiseConfig, int minimumYFloorDiv) {
+		final int startingChunkSectionIndex = chunk.getSectionIndex(
+				generationShapeHeightFloorDiv * generationShapeConfig.verticalCellBlockCount() - 1 + minimumY);
+		final int minimumYChunkSectionIndex = chunk.getSectionIndex(minimumY);
+		// Get the chunk section array from the chunk directly instead of constructing it manually
+		@NotNull final var chunkSections = chunk.getSectionArray();
+		for (int chunkSectionIndex = startingChunkSectionIndex; chunkSectionIndex >= minimumYChunkSectionIndex; --chunkSectionIndex) {
+			chunkSections[chunkSectionIndex].lock();
 		}
 
-		cir.setReturnValue(CompletableFuture.supplyAsync(Util.debugSupplier("wgen_fill_noise", () -> {
-			int startingChunkSectionIndex = chunk.getSectionIndex(
-					generationShapeHeightFloorDiv * generationShapeConfig.verticalCellBlockCount() - 1 + minimumY);
-			int minimumYChunkSectionIndex = chunk.getSectionIndex(minimumY);
-			var chunkSections = chunk.getSectionArray();
+		@Nullable Chunk chunkWithNoise;
+		try {
+			chunkWithNoise = this.populateNoise(
+					blender, structureAccessor, noiseConfig, chunk, minimumYFloorDiv, generationShapeHeightFloorDiv);
+		} finally {
+			// Replace an enhanced for loop with a fori loop and reuse the chunk sections array used when locking the chunk sections
 			for (int chunkSectionIndex = startingChunkSectionIndex; chunkSectionIndex >= minimumYChunkSectionIndex; --chunkSectionIndex) {
-				chunkSections[chunkSectionIndex].lock();
+				chunkSections[chunkSectionIndex].unlock();
 			}
+		}
 
-			Chunk chunkWithNoise;
-			try {
-				chunkWithNoise = this.populateNoise(
-						blender, structureAccessor, noiseConfig, chunk, minimumYFloorDiv, generationShapeHeightFloorDiv);
-			} finally {
-				for (int chunkSectionIndex = startingChunkSectionIndex; chunkSectionIndex >= minimumYChunkSectionIndex; --chunkSectionIndex) {
-					chunkSections[chunkSectionIndex].unlock();
-				}
-			}
-
-			return chunkWithNoise;
-		}), Util.getMainWorkerExecutor()));
+		return chunkWithNoise;
 	}
 }
--- common/src/main/java/io/github/steveplays28/noisium/mixin/compat/lithium/LithiumNoiseChunkGeneratorMixin.java
+++ common/src/main/java/io/github/steveplays28/noisium/mixin/compat/lithium/LithiumNoiseChunkGeneratorMixin.java
@@ -1,8 +1,6 @@
 package io.github.steveplays28.noisium.mixin.compat.lithium;
 
-import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.block.BlockState;
-import net.minecraft.util.Util;
 import net.minecraft.world.biome.source.BiomeSource;
 import net.minecraft.world.chunk.Chunk;
 import net.minecraft.world.chunk.ChunkSection;
@@ -12,14 +10,13 @@ import net.minecraft.world.gen.chunk.ChunkGenerator;
 import net.minecraft.world.gen.chunk.GenerationShapeConfig;
 import net.minecraft.world.gen.chunk.NoiseChunkGenerator;
 import net.minecraft.world.gen.noise.NoiseConfig;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-
-import java.util.concurrent.CompletableFuture;
 
 @Mixin(NoiseChunkGenerator.class)
 public abstract class LithiumNoiseChunkGeneratorMixin extends ChunkGenerator {
@@ -31,7 +28,7 @@ public abstract class LithiumNoiseChunkGeneratorMixin extends ChunkGenerator {
 	}
 
 	@Redirect(method = "populateNoise(Lnet/minecraft/world/gen/chunk/Blender;Lnet/minecraft/world/gen/StructureAccessor;Lnet/minecraft/world/gen/noise/NoiseConfig;Lnet/minecraft/world/chunk/Chunk;II)Lnet/minecraft/world/chunk/Chunk;", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/chunk/ChunkSection;setBlockState(IIILnet/minecraft/block/BlockState;Z)Lnet/minecraft/block/BlockState;"))
-	private BlockState noisium$populateNoiseWrapSetBlockStateOperation(ChunkSection chunkSection, int chunkSectionBlockPosX, int chunkSectionBlockPosY, int chunkSectionBlockPosZ, BlockState blockState, boolean lock) {
+	private BlockState noisium$populateNoiseWrapSetBlockStateOperation(@NotNull ChunkSection chunkSection, int chunkSectionBlockPosX, int chunkSectionBlockPosY, int chunkSectionBlockPosZ, @NotNull BlockState blockState, boolean lock) {
 		// Set the blockstate in the palette storage directly to improve performance
 		var blockStateId = chunkSection.blockStateContainer.data.palette.index(blockState);
 		chunkSection.blockStateContainer.data.storage().set(
@@ -42,38 +39,36 @@ public abstract class LithiumNoiseChunkGeneratorMixin extends ChunkGenerator {
 		return blockState;
 	}
 
-	@Inject(method = "populateNoise(Lnet/minecraft/world/gen/chunk/Blender;Lnet/minecraft/world/gen/noise/NoiseConfig;Lnet/minecraft/world/gen/StructureAccessor;Lnet/minecraft/world/chunk/Chunk;)Ljava/util/concurrent/CompletableFuture;", at = @At(value = "RETURN", shift = At.Shift.BEFORE), cancellable = true)
-	private void noisium$populateNoiseInject(Blender blender, NoiseConfig noiseConfig, StructureAccessor structureAccessor, Chunk chunk, CallbackInfoReturnable<CompletableFuture<Chunk>> cir, @Local GenerationShapeConfig generationShapeConfig, @Local(ordinal = 0) int minimumY, @Local(ordinal = 1) int minimumYFloorDiv, @Local(ordinal = 2) int generationShapeHeightFloorDiv) {
-		if (generationShapeHeightFloorDiv <= 0) {
-			cir.setReturnValue(CompletableFuture.completedFuture(chunk));
-			return;
+	/**
+	 * @author Steveplays28
+	 * @reason Improved chunk locking and unlocking speed by getting the chunk section array directly from the chunk
+	 * and by replacing {@code foreach} with {@code fori}.
+	 */
+	@Overwrite
+	public @Nullable Chunk method_38332(@NotNull Chunk chunk, int generationShapeHeightFloorDiv, @NotNull GenerationShapeConfig generationShapeConfig, int minimumY, @NotNull Blender blender, @NotNull StructureAccessor structureAccessor, @NotNull NoiseConfig noiseConfig, int minimumYFloorDiv) {
+		final int startingChunkSectionIndex = chunk.getSectionIndex(
+				generationShapeHeightFloorDiv * generationShapeConfig.verticalCellBlockCount() - 1 + minimumY);
+		final int minimumYChunkSectionIndex = chunk.getSectionIndex(minimumY);
+		// Get the chunk section array from the chunk directly instead of constructing it manually
+		@NotNull final var chunkSections = chunk.getSectionArray();
+		for (int chunkSectionIndex = startingChunkSectionIndex; chunkSectionIndex >= minimumYChunkSectionIndex; --chunkSectionIndex) {
+			chunkSections[chunkSectionIndex].lock();
 		}
 
-		cir.setReturnValue(CompletableFuture.supplyAsync(Util.debugSupplier("wgen_fill_noise", () -> {
-			int startingChunkSectionIndex = chunk.getSectionIndex(
-					generationShapeHeightFloorDiv * generationShapeConfig.verticalCellBlockCount() - 1 + minimumY);
-			int minimumYChunkSectionIndex = chunk.getSectionIndex(minimumY);
-			var chunkSections = chunk.getSectionArray();
+		@Nullable Chunk chunkWithNoise;
+		try {
+			chunkWithNoise = this.populateNoise(
+					blender, structureAccessor, noiseConfig, chunk, minimumYFloorDiv, generationShapeHeightFloorDiv);
+		} finally {
+			// Replace an enhanced for loop with a fori loop and reuse the chunk sections array used when locking the chunk sections
+			// Also run calculateCounts() on every chunk section to add Lithium compatibility
 			for (int chunkSectionIndex = startingChunkSectionIndex; chunkSectionIndex >= minimumYChunkSectionIndex; --chunkSectionIndex) {
-				chunkSections[chunkSectionIndex].lock();
+				@NotNull final var chunkSection = chunkSections[chunkSectionIndex];
+				chunkSection.calculateCounts();
+				chunkSection.unlock();
 			}
+		}
 
-			Chunk chunkWithNoise;
-			try {
-				chunkWithNoise = this.populateNoise(
-						blender, structureAccessor, noiseConfig, chunk, minimumYFloorDiv, generationShapeHeightFloorDiv);
-			} finally {
-				// Replace an enhanced for loop with a fori loop
-				// Also run calculateCounts() on every chunk section to add Lithium compatibility
-				for (int chunkSectionIndex = startingChunkSectionIndex; chunkSectionIndex >= minimumYChunkSectionIndex; --chunkSectionIndex) {
-					var chunkSection = chunkSections[chunkSectionIndex];
-
-					chunkSection.calculateCounts();
-					chunkSection.unlock();
-				}
-			}
-
-			return chunkWithNoise;
-		}), Util.getMainWorkerExecutor()));
+		return chunkWithNoise;
 	}
 }
